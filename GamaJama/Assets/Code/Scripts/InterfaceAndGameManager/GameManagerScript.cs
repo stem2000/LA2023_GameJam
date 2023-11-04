975f8c22d02ee16a0efe7afe3dc5d7f6046536a7
DenChikov <denis.kryuchkov.2000@mail.ru>
2023-11-04 16:27:50 +0300

    GameManagerAndUI

---
+++ GamaJama/Assets/Code/Scripts/InterfaceAndGameManager/GameManagerScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public event EventHandler OnAllUFOSDefeated;// for testing, deleted 
+
+    [SerializeField] private List<GameObject> menusList;
+    [SerializeField] private KeyCode pauseButton;
+    private void Start()
+    {
+        OnAllUFOSDefeated += WinPlayer;
+    }
+    private void PauseMenuView()
+    {
+        if (Input.GetKeyDown(pauseButton))
+        {
+            if (menusList[0].activeSelf == false)
+            {
+                Time.timeScale = 0;
+                menusList[0].SetActive(true);
+            }
+            else if (menusList[0].activeSelf == true)
+            {
+                Time.timeScale = 1;
+                menusList[0].SetActive(false);
+            }
+        }
+    }
+    private void DefeatPlayer(object sender, EventArgs e)
+    {
+        Time.timeScale = 0;
+        menusList[1].SetActive(true);
+    }
+    private void WinPlayer(object sender, EventArgs e)
+    {
+        Time.timeScale = 0;
+        menusList[2].SetActive(true);
+    }
+    private void Update()
+    {
+       PauseMenuView();
+    }
+
+}
